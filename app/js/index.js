5d4eaf3a00826aed9e95f4dc88805f69a1a1bddf
JailBreak <admin@vgee.cn>
2015-12-06 21:00:09 +0800

    完成前端动效 与切换逻辑

--- app/js/index.js
+++ app/js/index.js
@@ -14,7 +14,7 @@ var loadUserMsg = function(cb) {
         date = new Date();
         ipLoc = window.remote_ip_info;
 
-    data.text = $('.edit #msg').val() || ' ';
+    data.text = $('.edit #msg').val();
     data.location = ipLoc.city || ipLoc.province || '北京';
     data.time = date.getMonth() + '/' + date.getDate();
     var currentWeither = weitherAPI + data.location;
@@ -45,20 +45,55 @@ var init = function() {
 
     $('.submit').click(function(e) {
         e.preventDefault();
+        var user = Cookies.get('user');
+        if(!user) {
+            user = window.prompt('你叫什么呢？（小于10个字符)','');
+            if(!user) {
+                user = window.prompt('别淘气，你叫啥？','');
+            }
+            if(user.length > 10) {
+                user = window.prompt('名字太非主流不好（小于10个字符)', user);
+            }
+            Cookies.set('user', user, { expires: 1000, path: '/' });
+        }
+
+        if(!$('.edit #msg').val()) {
+            alert('总得说点什么吧');
+            return;
+        }
+
         $(this).addClass('circle')
+
         loadUserMsg(function(data) {
+
+            data.user = user;
             console.log(data);
             $(this).removeClass('circle');
             $('.edit').toHide().one(transitionend, function(){
-                $(this).hide();
+                $(this).unbind(transitionend).hide();
                 $('.preview').show().find('#prepre').text(data.text).end()
                     .find('#temp').text(data.weither).end()
                     .find('#time').text(data.time).end()
                     .find('#loc').text(data.location).end()
-                    .toShow()
+                    .find('.user span').text(data.user);
 
-            });;
+                setTimeout(function(){
+                    $('.preview').toShow(); 
+                });
+
+            });
+
         }.bind(this))
+    })
+
+    $('.ret').click(function() {
+        $('.preview').toHide().one(transitionend, function() {
+            $(this).unbind(transitionend).hide();
+            $('.edit').show() 
+            setTimeout(function(){
+                $('.edit').toShow(); 
+            })
+        })
     })
 }
 
@@ -68,3 +103,23 @@ $(function() {
 
     init();
 })
+
+// var test = function() {
+//     var arr = [];
+//     var str = 'http://openweathermap.org/img/w/';
+//     var html = '';
+//     for(i=1;i<999;i++) {
+//         var num = '';
+//         var ss = '';
+//         if(i<10) {
+//             num = '0' + i;
+//         } else {
+//             num = '' + i;
+//         }
+//         ss = str + num + 'd.png';
+//         html += '<img src="' + ss + '">\n';
+//     }
+//     console.log(html);
+//     $('#test').html(html);
+// }
+// test();
